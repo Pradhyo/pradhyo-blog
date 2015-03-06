8cc1d5e61c529c4bacb6ac019d5d3fd0a9cd7953
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-06 00:27:23 -0500

    Add form whose input is sent to a Google search

--- main.py
+++ main.py
@@ -16,9 +16,15 @@
 #
 import webapp2
 
+form = """<form action="http://www.google.com/search">
+<input name = "q">
+<input type = "submit">
+</form> """
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Under Construction')
+    	self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
