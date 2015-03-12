e85198a418f240a34383e0e38eb02ba2cff2e0e8
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-12 13:55:50 -0400

    Add a jinja environment

--- main.py
+++ main.py
@@ -17,6 +17,11 @@
 import webapp2
 import codecs
 import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment (loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 welcome = """ <b> Welcome to Pradhyo's blog </b>
 <br>
