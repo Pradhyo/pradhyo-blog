eb321b93226bd965fc5158220bc10946576ddf4c
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-12 14:49:52 -0400

    Add base Handler class with write and render functions

--- main.py
+++ main.py
@@ -50,6 +50,17 @@ rot13form = """ <b> Enter some text to ROT13: </b>
 <input type = "submit">
 </form> """
 
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self,template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/html'
