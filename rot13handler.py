14b17d3eefd92c808e9a8933a41c296e664cd6ad
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-15 22:14:05 -0400

    Add rot13handler to new file

---
+++ rot13handler.py
@@ -0,0 +1,13 @@
+from handler import Handler
+
+class Rot13Handler(Handler):
+	
+	def get(self):
+		self.response.headers['Content-Type'] = 'text/html'
+		self.render("rot13.html", text = "Enter text..")
+
+	def post(self):
+		string = self.request.get("text")
+		string = codecs.encode(string,'rot13')
+		self.response.headers['Content-Type'] = 'text/html'
+		self.render("rot13.html", text = string)
