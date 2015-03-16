1c67324a1e95bf489eed590e244b6fed408e22ed
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-15 22:12:43 -0400

    Add new file for MainHandler class

---
+++ mainhandler.py
@@ -0,0 +1,12 @@
+from handler import Handler
+
+class MainHandler(Handler):
+	def get(self):
+		self.response.headers['Content-Type'] = 'text/html'
+		self.render("Welcome.html")
+
+	def post(self):
+		if self.request.get("project") == "rot13":
+			self.redirect("/rot13")
+		else:
+			self.response.write("I am still working on some cool stuff :)")
