285d1ce1b6c5a1649065b0c995919ef3a950d2cd
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-29 23:37:21 -0400

    Link BlogHandler to /blog in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import webapp2
 from mainhandler import MainHandler
 from rot13handler import Rot13Handler
+from bloghandler import BlogHandler
 
 app = webapp2.WSGIApplication([('/', MainHandler),
-								('/rot13', Rot13Handler)], debug=True)
+							   ('/blog', BlogHandler),
+							   ('/rot13', Rot13Handler)], debug=True)
