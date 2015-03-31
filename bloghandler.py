a8b29cc7c8cc81d5ba0b45ff7a014268650612a1
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-30 23:39:16 -0400

    Create data model for each post; function to assign key to each post based on topic of blog

--- bloghandler.py
+++ bloghandler.py
@@ -4,3 +4,17 @@ class BlogHandler(Handler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/html'
 		self.render("BlogHome.html")
+
+def blog_key(topic = 'non-tech'):
+	"""Assign blog key with parent as topic """
+	return db.Key.from_path('blogs', topic)
+
+class Post(db.Model):
+	"""Defines each blog post """
+	title = db.StringProperty(required = True)
+	blog = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+	edited = db.DateTimeProperty(auto_now = True)
+
+
+
