2c303200b75a5e450ab7967ecfdf63d730f8bc9c
Pradhyo Bijja <pradhyo@nyu.edu>
2015-04-01 22:41:45 -0400

    Include handler for each post ie for /parent/PostID; Fix urls in app.yaml

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import webapp2
 from mainhandler import MainHandler
 from rot13handler import Rot13Handler
-from bloghandler import BlogHandler, NewPost
+from bloghandler import BlogHandler, NewPost, PostPage
 
 app = webapp2.WSGIApplication([('/', MainHandler),
 							   ('/blog', BlogHandler),
 							   ('/newpost', NewPost),
+                               ('/blog/([a-z]+)/([0-9]+)', PostPage),							   
 							   ('/rot13', Rot13Handler)], debug=True)
