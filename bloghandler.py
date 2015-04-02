3ca4a244074592b07e1dbeac3fe4788c194a8bf2
Pradhyo Bijja <pradhyo@nyu.edu>
2015-04-01 22:40:43 -0400

    Add Handler for individual posts based on parent/PostID

--- bloghandler.py
+++ bloghandler.py
@@ -10,7 +10,7 @@ class BlogHandler(Handler):
 			posts.ancestor(blog_key(category = category))
 		self.render("BlogHome.html", posts = posts, search = True)
 
-def blog_key(category = 'non-tech'):
+def blog_key(category = 'general'):
 	"""Assign blog key with parent as category """
 	return db.Key.from_path('blogs', category)
 
@@ -47,3 +47,14 @@ class NewPost(Handler):
 		else: 
 			error = "Enter both :/"
 			self.render("NewPost.html", title = title, blog = blog, error = error)
+
+class PostPage(Handler):
+    def get(self, category, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key(category=category))
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post = post)
