f7e56cf8196a2061267f076e17d24db70d0cc8d1
Pradhyo Bijja <pradhyo@nyu.edu>
2015-04-15 23:43:32 -0400

    Add resize image function while maintaining aspect ratio

--- misc_handler.py
+++ misc_handler.py
@@ -1,5 +1,16 @@
 from handler import Handler
 import codecs
+import Image
+
+def resize_image(img, max_width, max_height, crop = False):
+	"""Returns resized image not exceeding max_size while maintaining
+	aspect ratio """
+	width, height = img.size
+	if crop:
+		factor = max(max_width/float(width), max_height/float(height))
+	else:
+		factor = min(max_width/float(width), max_height/float(height))
+	return img.resize((int(width*factor),int(height*factor)), Image.ANTIALIAS)
 
 class Rot13Handler(Handler):
 	
