85f45015b01d89af4d061e5d8a3507d802cdd8ba
Pradhyo Bijja <pradhyo@nyu.edu>
2015-04-15 23:35:24 -0400

    Add handler for image resizing

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import webapp2
 from mainhandler import MainHandler
-from rot13handler import Rot13Handler
+from misc_handler import Rot13Handler, ImgResize
 from bloghandler import BlogHandler, NewPost, PostPage
 
 app = webapp2.WSGIApplication([('/', MainHandler),
 							   ('/blog', BlogHandler),
 							   ('/newpost', NewPost),
                                ('/blog/([a-z]+)/([0-9]+)', PostPage),							   
+                               ('/resizer', ImgResize),
 							   ('/rot13', Rot13Handler)], debug=True)
