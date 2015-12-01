108db811badda264f89d703165ec955bc8c994e1
Pradhyo Bijja <pradhyo@nyu.edu>
2015-11-30 23:52:51 -0500

    Add transaction api

--- main.py
+++ main.py
@@ -2,10 +2,30 @@ import webapp2
 from mainhandler import MainHandler
 from misc_handler import Rot13Handler, ImgResize
 from bloghandler import BlogHandler, NewPost, PostPage
+from handler import Handler, render_str
+from google.appengine.ext import db
+
+class Transaction(db.Model):
+	amount = db.IntegerProperty()
+	userid = db.IntegerProperty()
+
+class TransactionHandler(Handler):
+	def get(self):
+		transactions = Transaction.all()
+		self.render("Transactions.html", transactions = transactions)
+
+	def post(self):
+		amount = int(self.request.get('amount'))
+		userid = int(self.request.get('userid'))
+		temp_transaction = Transaction(amount=amount, userid=userid)
+		temp_transaction.put()
+		self.redirect('/api/transaction')
+
 
 app = webapp2.WSGIApplication([('/', MainHandler),
 							   ('/blog', BlogHandler),
 							   ('/newpost', NewPost),
-                               ('/blog/([a-z]+)/([0-9]+)', PostPage),							   
+                               ('/blog/([a-z]+)/([0-9]+)', PostPage),
                                ('/resizer', ImgResize),
-							   ('/rot13', Rot13Handler)], debug=True)
+							   ('/rot13', Rot13Handler),
+							   ('/api/transaction', TransactionHandler)], debug=True)
