6afbe8fc7df61724c99c65be629d4bd174b3ea47
Pradhyo Bijja <pradhyo@nyu.edu>
2016-02-28 02:01:04 -0500

    Call Vision api with image url

--- vision_handler.py
+++ vision_handler.py
@@ -2,6 +2,12 @@ from handler import Handler
 import requests
 import base64
 
+import argparse
+import httplib2
+
+from apiclient.discovery import build
+from oauth2client.client import GoogleCredentials
+
 class Vision(Handler):
 
 	def get(self):
@@ -9,7 +15,30 @@ class Vision(Handler):
 		self.render("vision.html")
 
 	def post(self):
+		API_DISCOVERY_FILE = 'https://vision.googleapis.com/$discovery/rest?version=v1'
+		http = httplib2.Http()
+
+		credentials = GoogleCredentials.get_application_default().create_scoped(['https://www.googleapis.com/auth/cloud-platform'])
+		credentials.authorize(http)
+
+		service = build('vision', 'v1', http, discoveryServiceUrl=API_DISCOVERY_FILE)
 		url = self.request.get("url")
 		response = requests.get(url).content
-		b = base64.b64encode(response)
-		self.render("vision.html", output = b)
+		image_content = base64.b64encode(response)
+		service_request = service.images().annotate(
+		  body={
+			'requests': [{
+			  'image': {
+				'content': image_content
+			   },
+			  'features': [{
+				'type': 'LABEL_DETECTION',
+				'maxResults': 5,
+			   }]
+			 }]
+		  })
+		response = service_request.execute()
+		label = response['responses'][0]['labelAnnotations'][0]['description']
+		# print('Found label: %s for %s' % (label, photo_file))
+		# return 0
+		self.render("vision.html", output = label)
